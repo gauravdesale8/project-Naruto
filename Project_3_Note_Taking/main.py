98c956c2ec7ebe6ae6360769667baf53f29c400e
gauravdesale8 <gauravdesale08@gmail.com>
2026-01-20 11:34:23 +0530

    add menu and loops

--- Project_3_Note_Taking/main.py
+++ Project_3_Note_Taking/main.py
@@ -12,8 +12,26 @@ def read_notes():
             print(line.strip())
 
 def main():
-    write_note()
-    read_notes()
+    while True:
+        print("\n----Notes App----")
+        print("""
+        1. Add a note,
+        2. View notes,
+        3. Exit
+        """)
+
+        choice = input("Choose an option: ")
+
+        if choice == '1':
+            write_note()
+        elif choice == '2':
+            read_notes()
+        elif choice == '3':
+            print("GoodBye.")
+            break
+        else:
+            print("Invalid choice. Try Again.")
+
 
 if __name__ == '__main__':
     main()
--- Project_3_Note_Taking/readme.md
+++ Project_3_Note_Taking/readme.md
@@ -1,1 +1,13 @@
 # Note Taking App
+
+## Step 1: File Basics (Reading & Writing)
+
+* A simple notes log stored in a text file.
+
+
+## Step 2: Menu + loop (Turning script into app)
+
+#### Goal:
+* Add a menu
+* Use a while loop
+* Let user choose actions
