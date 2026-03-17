de118aefe0c09d21286f11ab14a526fbccf7aac3
gauravdesale8 <gauravdesale08@gmail.com>
2026-03-17 23:14:15 +0530

    add part 3

--- Project_6_Expense_Tracker/main.py
+++ Project_6_Expense_Tracker/main.py
@@ -8,6 +8,21 @@ def show_total_expenses(expenses):
 
     print("\nTotal Spending:", total)
 
+def show_category_totals(expenses):
+    category_totals = {}
+
+    for e in expenses:
+        if e.category not in category_totals:
+            category_totals[e.category] = 0
+
+        category_totals[e.category] += e.amount
+
+    print("\n---- Category Total ----")
+
+    for category, total in category_totals.items():
+        print(category, ":",total)
+
+
 def main():
     expenses = []
 
@@ -15,7 +30,9 @@ def main():
         print("\n---Expenses Tracker---")
         print("1.Add Expenses")
         print("2.View Expenses")
-        print("3.Exit")
+        print("3. Total Spending")
+        print("4. Category Totals")
+        print("5. Exit")
 
         choice = input("Choose option: ").strip()
 
@@ -34,6 +51,12 @@ def main():
                 print(e.date,"|",e.amount,"|",e.category,"|",e.description)
 
         elif choice == "3":
+            show_total_expenses(expenses)
+
+        elif choice == "4":
+            show_category_totals(expenses)
+
+        elif choice == "5":
             break
 
         else:
